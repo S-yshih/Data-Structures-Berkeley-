3811e05a025643ba2628a4806bc3110f3ca272dc
Sabrina <120212758+S-yshih@users.noreply.github.com>
2023-03-30 21:54:02 -0400

    Completed both max methods, UNTESTED

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -2,20 +2,43 @@ package deque;
 
 
 import java.util.Comparator;
+import java.util.Iterator;
 
 public class MaxArrayDeque<T> extends ArrayDeque{
-
+    private Comparator<T> comparator;
 
     public MaxArrayDeque(Comparator<T> c){
         //creates max array deque with given comparator
+        super();
+        this.comparator = c;
     }
-/**
+
     public T max(){
         //returns max elem in the deque (return null if empty)
+        return max(this.comparator);
     }
 
     public T max(Comparator<T> c){
         //returns max elem governed by passed comparator (return null if empty)
+        T max = null;
+        T prev = null;
+        Iterator<T> it = iterator();
+        while (it.hasNext()){
+            T maxOfTwoItems;
+            if (c.compare(prev, it.next()) > 0){
+                maxOfTwoItems = prev;
+            } else { maxOfTwoItems = it.next();}
+
+            if (c.compare(maxOfTwoItems, max) > 0){
+                max = maxOfTwoItems;
+            }
+            prev = it.next();
+        }
+        return max;
     }
-**/
+    @Override
+    public boolean equals(Object o){
+        return false;
+    }
+
 }
--- proj1/deque/MaxArrayDequeTest.java
+++ proj1/deque/MaxArrayDequeTest.java
@@ -1,9 +1,30 @@
 package deque;
 
 import org.junit.Test;
+
+import java.util.Comparator;
+
 import static org.junit.Assert.*;
 
 public class MaxArrayDequeTest{
 
+    @Test
+    public void smallestMax(){
+        class LeastInteger implements Comparator<Integer> {
+            public int compare(Integer a, Integer b) {
+                if (a < b) return 1;
+                if (a.equals(b)) return 0;
+                return -1;
+            }
+            public Comparator<Integer> leastMaxComparator(){
+
+            }
+        }
+
+        MaxArrayDeque<Integer> list = new MaxArrayDeque<>(LeastInteger);
+
+    }
+    @Test
+    public void
 
 }
